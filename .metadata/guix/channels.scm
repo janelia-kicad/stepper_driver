db4ccf6a61d267976eefcf6493fb3305fa2394d8
Peter Polidoro <peter@polidoro.io>
2023-07-31 14:36:31 -0400

    Initial placement of tracks and zones

--- .metadata/guix/channels.scm
+++ .metadata/guix/channels.scm
@@ -5,7 +5,7 @@
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "67e22584faaa558c2a5834a5013d77660ec45e85")
+          "c963ba2ebb8097c475ba6e2b5a31be6678f2e74b")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
